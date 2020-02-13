af5f4ce746887482b062b3a1d783c0ad7d32fc72
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-14 00:17:00 +0530

    hello component with React.createElement

--- codevolution-youtube/hello-world/src/components/Hello.js
+++ codevolution-youtube/hello-world/src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Hello = () => {
-	return (
-		<div>
-			<h1>Hello SK.</h1>
-		</div>
-	);
-}
+	// return (
+	// 	<div>
+	// 		<h1>Hello SK.</h1>
+	// 	</div>
+	// );
+
+	// return React.createElement('div', null, 'h1', 'Hello SK.');
+	return React.createElement('div', null, React.createElement('h1', null, 'Hello SK.'));
+};
 
 export default Hello;
