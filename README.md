4a6644610a6766740f5575588fb4ea1b987d8f43
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-13 23:55:25 +0530

    template name customization

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ React - learning
 Tutorial
 
 1. [my-app](https://reactjs.org/tutorial/tutorial.html) -  tic-tac-toe game
+2. 
--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet';
+import MyGreet from './components/Greet';
 
 
 function App() {
 	return (
 		<div className="App">
-			<Greet/>
+			<MyGreet/>
 		</div>
 	);
 }
