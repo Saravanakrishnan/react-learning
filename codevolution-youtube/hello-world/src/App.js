d22a2d5fd49acd38ad5a9fdd933360551df4daef
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-14 23:34:26 +0530

    props

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-// import Greet from './components/Greet';
+import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 
 function App() {
 	return (
 		<div className="App">
-			{/*<Greet/>*/}
+			<Greet name={"John"} heroName={"Iron Man"}/>
+			<Greet name={"Flo"} heroName={"Captain America"}/>
+			<Greet name={"Ben"} heroName={"Thor"}/>
 			{/*<Welcome/>*/}
-			<Hello/>
+			{/*<Hello/>*/}
 		</div>
 	);
 }
