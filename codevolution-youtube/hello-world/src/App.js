fcbdc5cd45dd763f039ffb1981dd61ac13eff58d
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-14 23:40:18 +0530

    props as values without curly braces

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -7,13 +7,13 @@ import Hello from './components/Hello';
 function App() {
 	return (
 		<div className="App">
-			<Greet name={"John"} heroName={"Iron Man"}>
+			<Greet name="John" heroName="Iron Man">
 				<p>This is children props. </p>
 			</Greet>
-			<Greet name={"Flo"} heroName={"Captain America"}>
+			<Greet name="Flo" heroName="Captain America">
 				<button>Action</button>
 			</Greet>
-			<Greet name={"Ben"} heroName={"Thor"}/>
+			<Greet name="Ben" heroName="Thor"/>
 			{/*<Welcome/>*/}
 			{/*<Hello/>*/}
 		</div>
