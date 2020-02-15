aa8316f571125fc776bd5582a6315123a2295050
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-15 09:47:29 +0530

    states in class components

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from "./components/Message";
 
 function App() {
 	return (
 		<div className="App">
-			<Greet name="John" heroName="Iron Man">
-				<p>This is children props. </p>
-			</Greet>
-			<Greet name="Flo" heroName="Captain America">
-				<button>Action</button>
-			</Greet>
-			<Greet name="Ben" heroName="Thor"/>
+
+			<Message />
+
+			{/*<Greet name="John" heroName="Iron Man">*/}
+			{/*	<p>This is children props. </p>*/}
+			{/*</Greet>*/}
+			{/*<Greet name="Flo" heroName="Captain America">*/}
+			{/*	<button>Action</button>*/}
+			{/*</Greet>*/}
+			{/*<Greet name="Ben" heroName="Thor"/>*/}
 
 
-			<Welcome name="John" heroName="Iron Man"> </Welcome>
-			<Welcome name="Flo" heroName="Captain America"> </Welcome>
-			<Welcome name="Ben" heroName="Thor"> </Welcome>
+			{/*<Welcome name="John" heroName="Iron Man"> </Welcome>*/}
+			{/*<Welcome name="Flo" heroName="Captain America"> </Welcome>*/}
+			{/*<Welcome name="Ben" heroName="Thor"> </Welcome>*/}
 			{/*<Hello/>*/}
 		</div>
 	);
