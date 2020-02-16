ed5c68c7dafa1d5195ed4d521f99ecd8975eed09
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-16 22:26:44 +0530

    Destructure states in the class

--- codevolution-youtube/hello-world/src/components/Welcome.js
+++ codevolution-youtube/hello-world/src/components/Welcome.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 class Welcome extends Component {
 
 	render() {
-		const {name, heroName} = this.props;
+		const {name, heroName} = this.props; // destructuring props
+		// const {state1, state2} = this.state; // destructuring states
 		return <h1>Welcome {name} a.k.a {heroName} </h1>;
 	}
 }
