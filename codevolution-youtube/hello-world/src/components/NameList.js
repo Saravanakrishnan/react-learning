083fb07321af63fbeda3c0c216a0b76afa67a7b2
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-18 23:38:57 +0530

    list rendering - persons object

--- codevolution-youtube/hello-world/src/components/NameList.js
+++ codevolution-youtube/hello-world/src/components/NameList.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 function NameList(props) {
-	const names = ['Bruce', 'Clark', 'Diana'];
-	const nameList = names.map(name => <h2>{name}</h2>);
-	return <div>{nameList}</div> ;
+	const persons = [
+		{
+			id: 1,
+			name: 'Bruce',
+			age: 30,
+			skill: 'React'
+		},
+		{
+			id: 2,
+			name: 'Clark',
+			age: 25,
+			skill: 'Angular'
+		},
+		{
+			id: 3,
+			name: 'Diana',
+			age: 28,
+			skill: 'Vue'
+		}
+	];
+	const personList = persons.map(person => (
+			<h2>
+				I am {person.name}. I am {person.age} old. I know {person.skill}.
+			</h2>
+		)
+	);
+	return <div>{personList}</div>;
+	// const names = [, 'Clark', 'Diana'];
+	// const nameList = names.map(name => <h2>{name}</h2>);
+	// return <div>{nameList}</div>;
 }
 
 export default NameList;
