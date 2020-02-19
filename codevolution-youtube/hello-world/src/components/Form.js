5fe5bca2b993e72fd00b49854fdfa5c33fc470c7
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-19 23:34:42 +0530

    basics of form handling

---
+++ codevolution-youtube/hello-world/src/components/Form.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			comments: '',
+			topic: 'react'
+		}
+	}
+
+	handleUsernameChange = (event) => {
+		console.log(event.target.value);
+		this.setState({
+			username: event.target.value
+		});
+	};
+
+	handleCommentsChange = (event) => {
+		console.log(event.target.value);
+		this.setState({
+			comments: event.target.value
+		});
+	};
+
+	handleTopicChange = (event) => {
+		console.log(event.target.value);
+		this.setState({
+			topic: event.target.value
+		});
+	};
+
+	render() {
+
+		return (
+			<form>
+				<div>
+					<label>Username</label>
+					<input
+						type='text'
+						value={this.state.username}
+						onChange={this.handleUsernameChange}
+					/>
+				</div>
+				<div>
+					<label>Comments</label>
+					<textarea
+						value={this.state.comments}
+						onChange={this.handleCommentsChange}
+					/>
+				</div>
+				<div>
+					<label>Topic</label>
+					<select value={this.state.topic} onChange={this.handleTopicChange}>
+						<option value='React'>React</option>
+						<option value='Angular'>Angular</option>
+						<option value='Vue'>Vue</option>
+					</select>
+				</div>
+				<button type='submit'>Submit</button>
+			</form>
+		);
+	}
+}
+
+export default Form;
