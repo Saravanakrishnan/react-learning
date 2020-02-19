2bc82c2a905fda29952789dae0ae5d78f35e94fa
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-19 22:11:45 +0530

    styling: regular and module styles

--- codevolution-youtube/hello-world/src/components/Inline.js
+++ codevolution-youtube/hello-world/src/components/Inline.js
@@ -8,6 +8,8 @@ const heading = {
 function Inline() {
 	return (
 		<div>
+			<h1 className='error'>Error</h1> {/* will work, as styles is global */}
+			{/*<h1 className={styles.success}>Success</h1>*/} {/* will not work, as styles is module specific */}
 			<h1 style={heading}>Inline</h1>
 		</div>
 	);
