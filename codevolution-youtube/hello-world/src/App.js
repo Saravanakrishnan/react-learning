60dbd86c499d419d42f835354978a19e91d74429
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-21 00:09:07 +0530

    Fragments

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
 
 function App() {
 	return (
 		<div className="App">
-
+			<Table/>
+			{/*<FragmentDemo/>*/}
 		</div>
 	);
 }
