0bf91ef9f58ef2ffb88c1143c377ec0fb2a1483c
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-24 23:32:41 +0530

    Forwarding Refs

---
+++ codevolution-youtube/hello-world/src/components/FRParentInput.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import FrInput from "./FRInput";
+
+class FrParentInput extends Component {
+
+	constructor(props) {
+		super(props);
+		this.inputRef = React.createRef();
+	}
+
+	clickHandler = () => {
+		this.inputRef.current.focus() ;
+	}
+
+	render() {
+		return (
+			<div>
+				<FrInput ref={this.inputRef}/>
+				<button onClick={this.clickHandler}>Focus Input</button>
+			</div>
+		);
+	}
+}
+
+export default FrParentInput;
