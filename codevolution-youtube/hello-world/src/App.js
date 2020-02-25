bb08a94947d41c4670c32f2d9acc8fd0ee477d21
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-25 22:22:10 +0530

    cleanup

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import FrParentInput from "./components/FRParentInput";
-import PortalDemo from "./components/PortalDemo";
 
 function App() {
 	return (
 		<div className="App">
-			<PortalDemo/>
 		</div>
 	);
 }
