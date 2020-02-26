acd40a6dbfe33525a270382f4c816d911bc73162
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-26 23:21:03 +0530

    error boundary - basic impl

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
 	return (
 		<div className="App">
+			<ErrorBoundary>
+				<Hero heroname="Batman"/>
+				<Hero heroname="Superman"/>
+				<Hero heroname="Joker"/>
+			</ErrorBoundary>
 		</div>
 	);
 }
