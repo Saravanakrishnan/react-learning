d9e21d86b58da3250d2724e3f506d871c7fd0e85
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-27 11:18:42 +0530

    Render Props https://www.youtube.com/watch?v=EZil2OTyB4w&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3&index=37

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
+import CounterTwo from "./components/CounterTwo";
 import ClickCounterTwo from "./components/ClickCounterTwo";
 import HoverCounterTwo from "./components/HoverCounterTwo";
-import User from "./components/User";
 
 function App() {
 	return (
 		<div className="App">
-			<ClickCounterTwo/>
-			<HoverCounterTwo/>
-			<User render={(isLoggedIn) => isLoggedIn ? 'SK' : 'Guest'}/>
+			{/*<ClickCounterTwo/>*/}
+			{/*<HoverCounterTwo/>*/}
+			{/*<User render={(isLoggedIn) => isLoggedIn ? 'SK' : 'Guest'}/>*/}
+
+			<CounterTwo
+				render={(count, incrementCount) => (
+					<ClickCounterTwo
+						count={count}
+						incrementCount={incrementCount}>
+					</ClickCounterTwo>)}
+			/>
+			<CounterTwo
+				render={(count, incrementCount) => (
+					<HoverCounterTwo
+						count={count}
+						incrementCount={incrementCount}>
+					</HoverCounterTwo>)}
+			/>
 		</div>
 	);
 }
