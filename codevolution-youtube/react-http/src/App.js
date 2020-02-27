a0aaae81651ccdc715edf8dc3e8afaaba7cd6895
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-28 00:20:23 +0530

    http - simple http post request

--- codevolution-youtube/react-http/src/App.js
+++ codevolution-youtube/react-http/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import PostList from "./Components/PostList";
+import PostForm from "./Components/PostForm";
 
 function App() {
 	return (
 		<div className="App">
-			<PostList/>
+			<PostForm/>
 		</div>
 	);
 }
