fc1706740c3f4cacac8e7db34984f8ef7032ead5
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-27 11:09:24 +0530

    Render Props - a technique for sharing the code between React components using a prop whose value is a function

--- codevolution-youtube/hello-world/src/App.js
+++ codevolution-youtube/hello-world/src/App.js
@@ -9,7 +9,7 @@ function App() {
 		<div className="App">
 			<ClickCounterTwo/>
 			<HoverCounterTwo/>
-			<User name={(isLoggedIn) => isLoggedIn ? 'SK' : 'Guest'}/>
+			<User render={(isLoggedIn) => isLoggedIn ? 'SK' : 'Guest'}/>
 		</div>
 	);
 }
