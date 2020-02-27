c39d85474b2a39e896fa1684fca3c2b831bcdc96
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-27 23:27:29 +0530

    Context - provide a context value

---
+++ codevolution-youtube/react-context/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ComponentC from "./Components/ComponentC";
+import {UserProvider} from "./Components/UserContent";
+
+function App() {
+	return (
+		<div className="App">
+			<UserProvider value="SK">
+				<ComponentC/>
+			</UserProvider>
+		</div>
+	);
+}
+
+export default App;
