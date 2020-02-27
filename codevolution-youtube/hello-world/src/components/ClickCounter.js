4f20f62a5847daaedc73761323102141f5fbc917
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-27 10:00:15 +0530

    Higher order components - solution

--- codevolution-youtube/hello-world/src/components/ClickCounter.js
+++ codevolution-youtube/hello-world/src/components/ClickCounter.js
@@ -1,25 +1,13 @@
 import React, {Component} from 'react';
+import UpdatedComponent from './withCounter'
 
 class ClickCounter extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			count: 0
-		}
-	}
-
-
 	render() {
-		const {count} = this.state;
-		return <button onClick={this.incrementCount}> Clicked {count} times</button>
+		const {count, incrementCount} = this.props;
+		return <button onClick={incrementCount}> {count} times</button>
 	}
 
-	incrementCount = () => {
-		this.setState(prevState => {
-			return {count: prevState.count + 1}
-		})
-	};
 }
 
-export default ClickCounter;
+export default UpdatedComponent(ClickCounter);
