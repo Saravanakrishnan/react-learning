91f1597080d37cc6d872514bc956e58562ff38a4
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-02-28 21:24:37 +0530

    hooks - counter example - using useState with previous state

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import ClassCounter from "./Components/ClassCounter";
-import HookCounter from "./Components/HookCounter";
+import HookCounterTwo from "./Components/HookCounterTwo";
 
 function App() {
 	return (
 		<div className="App">
-			<ClassCounter/>
-			<HookCounter/>
+			<HookCounterTwo/>
 		</div>
 	);
 }
