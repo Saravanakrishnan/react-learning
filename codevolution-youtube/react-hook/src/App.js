6b4524acf4500269c94d86a9ddb80140075e883c
Saravanakrishnan Pk <saravanakrishnan01@gmail.com>
2020-03-02 23:10:26 +0530

    hooks - useEffect with incorrect dependency

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import MouseContainer from "./Components/MouseContainer";
+import IntervalClassCounter from "./Components/IntervalClassCounter";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
 
 function App() {
 	return (
 		<div className="App">
-			<MouseContainer/>
+			<IntervalClassCounter/>
+			<IntervalHookCounter/>
 		</div>
 	);
 }
