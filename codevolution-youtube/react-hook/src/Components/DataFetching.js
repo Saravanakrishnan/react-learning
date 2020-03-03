43e27a600b9df9d498bf1fefa713b3bf1ea24ed1
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-03 23:10:20 +0530

    hooks - Fetching data with useEffect Part 3

--- codevolution-youtube/react-hook/src/Components/DataFetching.js
+++ codevolution-youtube/react-hook/src/Components/DataFetching.js
@@ -6,6 +6,7 @@ function DataFetching() {
 	// const [posts, setPosts] = useState([]);
 	const [post, setPost] = useState({});
 	const [id, setId] = useState(1);
+	const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
 	// useEffect(() => {
 	// 	axios
@@ -18,21 +19,27 @@ function DataFetching() {
 	// 		});
 	// }, []);
 
+
+	const handleClick = () => {
+		setIdFromButtonClick(id);
+	}
+
 	useEffect(() => {
 		axios
-			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+			.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
 			.then(res => {
 				console.log(res);
 				setPost(res.data);
 			}).catch(err => {
 				console.log(err)
 			});
-	}, [id]);
+	}, [idFromButtonClick]);
 
 	return (
 		<div>
 
-			<input type='text' value={id} onChange={e => setId(e.target.value)}/>
+			<input type='text' value={idFromButtonClick} onChange={e => setId(e.target.value)}/>
+			<button type='button' onClick={handleClick}>Fetch Post</button>
 			<div>{post.title}</div>
 			{/*<ul>*/}
 			{/*	{*/}
