3a30e6f5bd526735491415c65058d12c078404e8
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-03 23:05:30 +0530

    hooks - Fetching data with useEffect Part 2

--- codevolution-youtube/react-hook/src/Components/DataFetching.js
+++ codevolution-youtube/react-hook/src/Components/DataFetching.js
@@ -3,25 +3,42 @@ import axios from 'axios';
 
 function DataFetching() {
 
-	const [posts, setPosts] = useState([]);
+	// const [posts, setPosts] = useState([]);
+	const [post, setPost] = useState({});
+	const [id, setId] = useState(1);
+
+	// useEffect(() => {
+	// 	axios
+	// 		.get('https://jsonplaceholder.typicode.com/posts')
+	// 		.then(res => {
+	// 			console.log(res);
+	// 			setPosts(res.data);
+	// 		}).catch(err => {
+	// 			console.log(err)
+	// 		});
+	// }, []);
+
 	useEffect(() => {
 		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
+			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
 			.then(res => {
 				console.log(res);
-				setPosts(res.data);
+				setPost(res.data);
 			}).catch(err => {
 				console.log(err)
 			});
-	}, []);
+	}, [id]);
 
 	return (
 		<div>
-			<ul>
-				{
-					posts.map(post => <li key={post.id}>{post.title}</li>)
-				}
-			</ul>
+
+			<input type='text' value={id} onChange={e => setId(e.target.value)}/>
+			<div>{post.title}</div>
+			{/*<ul>*/}
+			{/*	{*/}
+			{/*		posts.map(post => <li key={post.id}>{post.title}</li>)*/}
+			{/*	}*/}
+			{/*</ul>*/}
 		</div>
 	);
 }
