b0f800e2b557c1c17413a343e169dfe93b7ae0c0
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-03 22:59:20 +0530

    hooks - Fetching data with useEffect Part 1

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import IntervalClassCounter from "./Components/IntervalClassCounter";
-import IntervalHookCounter from "./Components/IntervalHookCounter";
+import DataFetching from "./Components/DataFetching";
 
 function App() {
 	return (
 		<div className="App">
-			<IntervalClassCounter/>
-			<IntervalHookCounter/>
+			<DataFetching/>
 		</div>
 	);
 }
