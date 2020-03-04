f5368927cbcabbcebee41feb9b9d989775638ffa
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-04 23:09:02 +0530

    hooks - useReducer - simple state & action

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './App.css';
 import ComponentC from "./Components/useContext/ComponentC";
+import CounterOne from "./Components/useReducer/CounterOne";
 
 // Step 1:
 export const UserContext = React.createContext();
 export const ChannelContext = React.createContext();
 
+// function App() {
+// 	return (
+// 		<div className="App">
+//
+// 			{/*// Step2: Wrap component*/}
+// 			<UserContext.Provider value={'SK'}>
+// 				<ChannelContext.Provider value={'Code Evolution'}>
+// 					<ComponentC/>
+// 				</ChannelContext.Provider>
+// 			</UserContext.Provider>
+// 		</div>
+// 	);
+// }
+
 function App() {
 	return (
 		<div className="App">
-
-			{/*// Step2: Wrap component*/}
-			<UserContext.Provider value={'SK'}>
-				<ChannelContext.Provider value={'Code Evolution'}>
-					<ComponentC/>
-				</ChannelContext.Provider>
-			</UserContext.Provider>
+			<CounterOne/>
 		</div>
 	);
 }
-
 export default App;
