85c2c09d4d62777a81fed37d1fc367d34e5bb49b
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-04 23:14:35 +0530

    hooks - useReducer - complex state & action

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import ComponentC from "./Components/useContext/ComponentC";
 import CounterOne from "./Components/useReducer/CounterOne";
+import CounterTwo from "./Components/useReducer/CounterTwo";
 
 // Step 1:
 export const UserContext = React.createContext();
@@ -24,7 +25,8 @@ export const ChannelContext = React.createContext();
 function App() {
 	return (
 		<div className="App">
-			<CounterOne/>
+			{/*<CounterOne/>*/}
+			<CounterTwo/>
 		</div>
 	);
 }
