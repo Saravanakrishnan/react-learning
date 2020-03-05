4392a897c277da5cb91fcb2f5e351abefd965162
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-05 09:17:50 +0530

    hooks - Fetching data with useReducer Part 1

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,36 +1,12 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import './App.css';
-import ComponentA from "./Components/useReducerAnduseContext/ComponentA";
-import ComponentB from "./Components/useReducerAnduseContext/ComponentB";
-import ComponentC from "./Components/useReducerAnduseContext/ComponentC";
-
-export const CountContext = React.createContext();
-
-const initialState = 0;
-const reducer = (state, action) => {
-	switch (action) {
-		case 'increment':
-			return state + 1;
-		case 'decrement':
-			return state - 1;
-		case 'reset':
-			return initialState;
-		default:
-			return state;
-	}
-};
+import DataFetching from "./Components/DataFetching/DataFetching";
 
 function App() {
-	const [count, dispatch] = useReducer(reducer, initialState);
 	return (
-		<CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
-			<div className="App">
-				count - {count}
-				<ComponentA/>
-				<ComponentB/>
-				<ComponentC/>
-			</div>
-		</CountContext.Provider>
+		<div className="App">
+			<DataFetching/>
+		</div>
 	);
 }
 
