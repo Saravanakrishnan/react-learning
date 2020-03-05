92073bc0100a0868c341a57ade28dd118292b841
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-05 09:07:41 +0530

    hooks - useReducer with useContext for Global state management

---
+++ codevolution-youtube/react-hook/src/Components/useReducerAnduseContext/ComponentE.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComponentF from "./ComponentF";
+
+function ComponentE() {
+	return (
+		<div>
+			Component E
+			<ComponentF/>
+
+		</div>
+	);
+}
+
+export default ComponentE;
