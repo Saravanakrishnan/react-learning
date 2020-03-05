75f0be3507ddb1f5866e331d2fe2fd0d5c625f83
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-05 08:36:32 +0530

    hooks - useReducer - Multiple useReducers - with same reducer functions

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import './App.css';
-import ComponentC from "./Components/useContext/ComponentC";
-import CounterOne from "./Components/useReducer/CounterOne";
-import CounterTwo from "./Components/useReducer/CounterTwo";
-
-// Step 1:
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
-
-// function App() {
-// 	return (
-// 		<div className="App">
-//
-// 			{/*// Step2: Wrap component*/}
-// 			<UserContext.Provider value={'SK'}>
-// 				<ChannelContext.Provider value={'Code Evolution'}>
-// 					<ComponentC/>
-// 				</ChannelContext.Provider>
-// 			</UserContext.Provider>
-// 		</div>
-// 	);
-// }
+import CounterThree from "./Components/useReducer/CounterThree";
 
 function App() {
 	return (
 		<div className="App">
-			{/*<CounterOne/>*/}
-			<CounterTwo/>
+			<CounterThree/>
 		</div>
 	);
 }
+
 export default App;
