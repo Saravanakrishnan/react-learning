d7abade6d72ed8286cc4a4c7d2e4c07abecb439f
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-06 08:18:29 +0530

    hooks - useRef - access dom nodes directly

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Counter from "./Components/useMemo/Counter";
+import FocusInput from "./Components/useRef/FocusInput";
 
 function App() {
 	return (
 		<div className="App">
-			<Counter/>
+			<FocusInput/>
 		</div>
 	);
 }
