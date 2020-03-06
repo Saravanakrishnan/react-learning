100c24d3c5cb44c09380123a32ea5885d80474e4
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-06 09:02:29 +0530

    hooks - custom hooks - example 2 - counter - useCounter.js

--- codevolution-youtube/react-hook/src/App.js
+++ codevolution-youtube/react-hook/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import CounterOne from "./Components/customHook/CounterOne";
+import CounterTwo from "./Components/customHook/CounterTwo";
 
 function App() {
 	return (
--- codevolution-youtube/react-hook/src/Components/customHook/CounterTwo.js
+++ codevolution-youtube/react-hook/src/Components/customHook/CounterTwo.js
@@ -1,20 +1,9 @@
 import React, {useState} from 'react';
+import useCounter from "../../hooks/useCounter";
 
 function CounterTwo() {
 
-	const [count, setCount] = useState(0);
-
-	const increment = () => {
-		setCount(prevCount => prevCount + 1)
-	};
-
-	const decrement = () => {
-		setCount(prevCount => prevCount - 1)
-	};
-
-	const reset = () => {
-		setCount(0)
-	};
+	const [count, increment, decrement, reset] = useCounter();
 
 	return (
 		<div>
