dc0359afc9758733d16f210e9726da63b71425ca
Saravanakrishnan Pk <8683623+saravanakrishnan@users.noreply.github.com>
2020-03-06 08:52:44 +0530

    hooks - custom hooks - useDocumentTitle

--- codevolution-youtube/react-hook/src/Components/customHook/DocTitleOne.js
+++ codevolution-youtube/react-hook/src/Components/customHook/DocTitleOne.js
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import useDocumentTitle from "../../hooks/useDocumentTitle";
 
 function DocTitleOne() {
 
 	const [count, setCount] = useState(0);
-	useEffect(() => {
-		document.title = `Count ${count}`;
-	}, [count]);
+
+	useDocumentTitle(count);
 
 	return (
 		<div>
---
+++ codevolution-youtube/react-hook/src/hooks/useDocumentTitle.js
@@ -0,0 +1,11 @@
+import {useEffect} from 'react';
+
+function UseDocumentTitle(count) {
+
+	useEffect(() => {
+		document.title = `Count ${count}`;
+	}, [count]);
+
+}
+
+export default UseDocumentTitle;
